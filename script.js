edd4fa98e10b50be6ae883c5ab8d2e9466ade955
Anisha Desktop <anisharanjan55@gmail.com>
2021-12-16 13:58:20 -0500

    toggle menu

--- script.js
+++ script.js
@@ -8,5 +8,9 @@ $(document).ready(function(){
         {
             $('.navbar').removeClass("sticky");
         }
-    })
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+    });
 });
