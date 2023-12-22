ade73f861334d2509cc2ecc2b49fcec4d1c7641b
AnishaRan <anisharanjan55@gmail.com>
2023-12-22 12:49:47 -0500

    replaced resume button with icons, added tooltips, updated cards, fixed navbar bug

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function closeMenu() {
+    // Select the menu and remove the 'active' class
+    $(".navbar .menu").removeClass("active");
+    $(".menu-btn i").removeClass("active");
+}
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20)
@@ -9,13 +14,16 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
     });
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+
     // toggle menu bars (new)
     function myFunction(x) {
         x.classList.toggle("change");
       }
+    $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    });
+    $(".navbar .menu li a").click(function () {
+        closeMenu();
+    });
 });
